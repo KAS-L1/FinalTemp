318bb28f882b99cbbb98a79bc60d5f1e56fafacd
KAS-L1 <kasl.54370906@gmail.com>
2024-12-29 08:19:31 +0800

    updated layout

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -98,18 +98,18 @@
                 elem[l].classList.add('active');
             }
         }
-        document.querySelector('#compo-menu-search').addEventListener('keyup', function() {
-            var tval = document.querySelector('#compo-menu-search').value.toLowerCase();
-            var elem = document.querySelectorAll('.component-list-card a');
-            for (var l = 0; l < elem.length; l++) {
-                var aval = elem[l].innerHTML.toLowerCase();
-                var n = aval.indexOf(tval);
-                if (n !== -1) {
-                    elem[l].style.display = 'block';
-                } else {
-                    elem[l].style.display = 'none';
-                }
-            }
+        // document.querySelector('#compo-menu-search').addEventListener('keyup', function() {
+        //     var tval = document.querySelector('#compo-menu-search').value.toLowerCase();
+        //     var elem = document.querySelectorAll('.component-list-card a');
+        //     for (var l = 0; l < elem.length; l++) {
+        //         var aval = elem[l].innerHTML.toLowerCase();
+        //         var n = aval.indexOf(tval);
+        //         if (n !== -1) {
+        //             elem[l].style.display = 'block';
+        //         } else {
+        //             elem[l].style.display = 'none';
+        //         }
+        //     }
         });
     </script>
     <script src="{{ asset('/assets/js/plugins/sweetalert2.all.min.js') }}"></script>
